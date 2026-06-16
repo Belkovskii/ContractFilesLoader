ba90123a4792e731ab4b886ceade10a4617ab73e
Denis <belkovsky.denis@yandex.ru>
2026-06-16 17:42:36 +0300

    try parse excel first row

--- Program.cs
+++ Program.cs
@@ -1,11 +1,21 @@
 ﻿
+using ClosedXML.Excel;
 using ContractLoader.Interfaces;
 
-var pathToExcelFile = "";
-IExcelParser parser;
-var excelRecords = parser.Parse(pathToExcelFile);
+using var workbook = new XLWorkbook("C:/xlsx_files/Файлы по документам Январь2026-Декабрь2026.xlsx");
+var worksheet = workbook.Worksheet(1);
+var firstDataRow = worksheet.Row(1);
+
+Console.WriteLine("Содержимое первой строки и номера столбцов:");
+
+foreach (var cell in firstDataRow.CellsUsed())
+{    
+    int columnNumber = cell.Address.ColumnNumber;
+    Console.WriteLine($"Столбец: {columnNumber}, Значение: {cell.Value}");
+}
 
 static void Proceed(IExcelParser parser)
 {
-
+    var pathToExcelFile = "";
+    var excelRecords = parser.Parse(pathToExcelFile);
 }
