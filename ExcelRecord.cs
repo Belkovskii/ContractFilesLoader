c14e249356f72983c3dd4ecad44f8c1c7f4fcb85
Denis <belkovsky.denis@yandex.ru>
2026-06-16 16:39:12 +0300

    first commit

---
+++ ExcelRecord.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContractLoader
+{
+    public class ExcelRecord
+    {
+        public string? FileGuid {  get; set; }
+        public string? DocumentGuid { get; set; }
+        public string? PathToFile { get; set; }
+        public string? FileModifiedDate { get; set; }
+        public string? FileModifiedUser { get; set; }        
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿
+using ContractLoader.Interfaces;
+
+var pathToExcelFile = "";
+IExcelParser parser;
+var excelRecords = parser.Parse(pathToExcelFile);
+
+static void Proceed(IExcelParser parser)
+{
+
+}
