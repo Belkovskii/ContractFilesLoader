257a68a079b97e869191574146dfe639e911348d
unknown <belkosky.denis@yandex.ru>
2026-06-17 07:47:39 +0300

    main functin v2@

--- Program.cs
+++ Program.cs
@@ -1,38 +1,48 @@
 ﻿
 using ContractLoader;
 using ContractLoader.ExcelParser;
-using ContractLoader.Interfaces;
 using System.Collections.Concurrent;
 
-
-using (ExcelParseHelper parser = new("C:\\xlsx_files\\январь-декабрь2026.xlsx"))
+class Program
 {
-    await Proceed(parser);
-}
-
-static async Task Proceed(ExcelParseHelper parser)
-{
-    var parallelOptions = new ParallelOptions { MaxDegreeOfParallelism = 50};
-       
-    var excelRecords = parser.Parse();
-    
-    var resultsConcurrentDictionary = new ConcurrentDictionary<string, string>();
-
-    await Parallel.ForEachAsync(excelRecords, parallelOptions, async (excelRecord, cancellationToken) =>
+    static async void Main(string[] args)
     {
-        if (excelRecord.FileGuid is string)
+        using (ExcelParseHelper parser = new("C:\\xlsx_files\\январь-декабрь2026.xlsx"))
         {
-            var processResult = await ProcessRecord(excelRecord);
-            resultsConcurrentDictionary.TryAdd(excelRecord.FileGuid, processResult);
-        }        
-    });
-    
-    parser.UpdateRecords(resultsConcurrentDictionary);
+            await Proceed(parser);
+        }
+    }
+
+    static async Task Proceed(ExcelParseHelper parser)
+    {
+        var parallelOptions = new ParallelOptions { MaxDegreeOfParallelism = 50 };
+
+        var excelRecords = parser.Parse();
+
+        var resultsConcurrentDictionary = new ConcurrentDictionary<string, string>();
+
+        await Parallel.ForEachAsync(excelRecords, parallelOptions, async (excelRecord, cancellationToken) =>
+        {
+            if (excelRecord.FileGuid is string)
+            {
+                var processResult = await ProcessRecord(excelRecord);
+                resultsConcurrentDictionary.TryAdd(excelRecord.FileGuid, processResult);
+            }
+        });
+
+        parser.UpdateRecords(resultsConcurrentDictionary);
+    }
+
+    static async Task<string> ProcessRecord(ExcelRecord record)
+    {
+        //TODO
+        return "success";
+    }
+
 }
 
-static async Task<string> ProcessRecord(ExcelRecord record)
-{
-    //TODO
-    return "success";
-}
+
+
+
+
 
